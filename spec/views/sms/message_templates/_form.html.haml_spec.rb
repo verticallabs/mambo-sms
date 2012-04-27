b7dbae15a0e046b2f00cc4267905e4668a281ec3
Paul Schuegraf <paul@verticallabs.ca>
2012-04-26 21:27:02 -0700

    remove extraneous print

--- spec/views/sms/message_templates/_form.html.haml_spec.rb
+++ spec/views/sms/message_templates/_form.html.haml_spec.rb
@@ -5,7 +5,6 @@ describe "sms/message_templates/_form" do
     m = FactoryGirl.create(:message_template)
     assign(:message_template, m)
     render
-    puts rendered.native
     rendered.should have_field('message_template_desc', :with => m.desc)
     rendered.should have_field('message_template_body', :text => m.body)
     rendered.should have_field('', :type => 'submit')
