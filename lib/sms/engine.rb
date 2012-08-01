57baba69d005e9cb0a0c812600dcdaa879e051f0
Chris Dion <chris@verticallabs.ca>
2012-08-01 14:23:18 -0700

    arg

--- lib/sms/engine.rb
+++ lib/sms/engine.rb
@@ -2,6 +2,12 @@ module Sms
   class Engine < ::Rails::Engine
     isolate_namespace Sms
 
+    initializer "models" do
+      require "sms/message"
+      require "sms/subscriber"
+      require "sms/message_template"
+    end
+
 		#
     initializer "i18n" do
     	I18n.load_path += Dir[File.join(__FILE__, "config", "locales", "**", "*.{rb,yml}")]
--- lib/sms/version.rb
+++ lib/sms/version.rb
@@ -1,4 +1,4 @@
 module Sms
-  VERSION = "0.0.18"
+  VERSION = "0.0.19"
   DM_VERSION = "~> 1.2"
 end
